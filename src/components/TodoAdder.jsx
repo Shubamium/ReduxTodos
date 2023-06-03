97cbbfb406d26c8b3472ec53bed19173746cccfa
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-03 15:03:57 +0700

    Add the base component for displaying the list

---
+++ src/components/TodoAdder.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TodoAdder = () => {
+  return (
+    <div>TodoAdder</div>
+  )
+}
---
+++ src/components/TodoView.jsx
@@ -0,0 +1,27 @@
+
+export const TodoView = (props) => {
+    function renderList(list){
+        return list.map((task)=>{
+            return (
+                <List title={task.title} description={task.description}></List>
+            )
+        })
+    }
+    return (
+        <div>
+            <h2>To-Do List</h2>
+            {props.list && renderList(props.list)}
+        </div>
+    )
+}
+
+
+
+const List = (props) => {
+  return (
+    <div>
+        <h2>${props.title}</h2>
+        <h2>${props.description}</h2>
+    </div>
+  )
+}
