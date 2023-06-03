18075e85b50438649f5c1c7c8e31f9e2d9883932
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-03 15:34:03 +0700

    Add form handling for adding todos with react-hook-form

--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux';
 
 export const TodoAdder = () => {
-  return (
-    <div>
-        TodoAdder
-
-    </div>
-  )
+    const {register,handleSubmit} = useForm();
+    // const dispatch = useDispatch();
+    const onSubmit = (d)=>{
+    }
+    return (
+        <div>
+            <h1>TodoAdder</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='form-field'>
+                    <label htmlFor="title">Task Name: </label>
+                    <input type="text" {...register("title",{required:true})} placeholder='What do you need to do?' />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="description" placeholder='Describe the task in detail here!'>Description:</label>
+                    <textarea {...register("description")}></textarea>
+                </div>
+                <button type='submit'>Add</button>
+            </form>
+        </div>
+    )
 }
